502cef6d8e0696a58c18871e683e74bd3fcc0b40
Jose Alfu <josealfu@gmail.com>
2022-05-08 19:46:15 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Citas</h1>
+  <label for="buscar">Buscar por numero de placa </label>
+  <input v-model="this.buscar" type="text" name="buscar" placeholder="Ingrese su placa">
+  <button @click="getCitas">Buscar</button>
+  <button @click="isAgendar = true" class="block-button">AGENDAR CITA</button>
+  <hr>
+  <p v-if="cita != '' && !isAgendar">{{ cita }}</p>
+  <AgendarComponent v-if="isAgendar" @citaAgendada="citaAgendada()" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AgendarComponent from "./components/AgendarComponent";
+
+const URL = 'https://citavehiculosapi2022.azurewebsites.net/'
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      buscar: '',
+      isAgendar: false,
+      cita: ''
+    }
+  },
+
+  methods: {
+    citaAgendada() {
+      alert("Cita agendada exitosamente.")
+      this.isAgendar = false
+    },
+
+    async getCitas() {
+      this.isAgendar = false
+      const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      }
+      try {
+        const data = await fetch(URL + 'api/Citas/placa/' + this.buscar, requestOptions)
+          .then(response => response.json())
+        const dateArray = data.fecha.split('T')
+        this.cita = 'Placa: ' + data.placa + ' Fecha: ' + dateArray[0] + ' ' + this.convertirHora(dateArray[1])
+      } catch (error) {
+        this.cita = 'Placa no encontrada.'
+      }
+    },
+    convertirHora(time) {
+      time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+      if (time.length > 1) {
+        time = time.slice(1);
+        time[5] = +time[0] < 12 ? 'AM' : 'PM';
+        time[0] = +time[0] % 12 || 12;
+      }
+      return time.join('');
+    }
+  },
+
   components: {
-    HelloWorld
+    AgendarComponent
   }
 }
 </script>
@@ -22,5 +72,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.block-button {
+  margin: auto;
+  margin-top: 10px;
+  width: 20%;
+  display: block;
 }
 </style>
---
+++ src/components/AgendarComponent.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <h2>Agendar Cita</h2>
+    <label for="placa">Placa: </label>
+    <input v-model="placa" name="placa" type="text" placeholder="placa">
+
+    <label for="fecha">Fecha: </label>
+    <input v-model="inputDate" type="date" id="date" :min="minDate" name="fecha">
+
+    <label for="hora">Hora: </label>
+    <select v-model="inputHour" name="hora" id="cars">
+      <option value="01">01</option>
+      <option value="02">02</option>
+      <option value="03">03</option>
+      <option value="04">04</option>
+      <option value="05">05</option>
+      <option value="06">06</option>
+      <option value="07">07</option>
+      <option value="08">08</option>
+      <option value="09">09</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+    </select>
+
+    <select v-model="inputMinutes" name="minutos" id="minutos">
+      <option value="00">00</option>
+      <option value="30">30</option>
+    </select>
+
+    <select v-model="inputMeridiano" name="meridiano" id="meridiano">
+      <option value="am">am</option>
+      <option value="pm">pm</option>
+    </select>
+
+    <button @click="postCita">Agendar</button>
+
+  </div>
+</template>
+
+<script>
+const URL = 'https://citavehiculosapi2022.azurewebsites.net/'
+
+export default {
+
+  beforeMount() {
+    this.getDate()
+  },
+
+  data() {
+    return {
+      placa: '',
+      fecha: '',
+      minDate: '',
+      inputDate: '',
+      inputHour: '',
+      inputMinutes: '',
+      inputMeridiano: ''
+    }
+  },
+
+  methods: {
+    getDate() {
+      let date = new Date()
+      this.minDate = date.toISOString().split('T')[0]
+    },
+    async postCita() {
+
+      const cita = {
+        "id": 0,
+        "fecha": new Date(
+          `${this.inputDate} ${this.inputHour}:${this.inputMinutes} ${this.inputMeridiano}`
+        ).toJSON(),
+        "atendida": false,
+        "placa": this.placa
+      }
+
+      console.log('CITA', cita)
+
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cita)
+      }
+      try {
+        await fetch(URL + 'api/Citas', requestOptions)
+          .then(response => response.json())
+        this.$emit('citaAgendada')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+label {
+  margin-left: 10px;
+}
+
+button {
+  margin-left: 10px;
+}
+</style>
