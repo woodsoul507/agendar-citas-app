c3ce5dd3fa09859782ef85123de0205aa9376931
Jose Alfu <josealfu@gmail.com>
2022-05-08 21:32:20 -0500

    added deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/citas-vue-app/',
   devServer: {
     proxy: 'https://citavehiculosapi2022.azurewebsites.net/api/Citas/',
 }
