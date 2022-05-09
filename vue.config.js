455f4e658499c34c11db11e37c1e13f72e8a1f5a
Jose Alfu <josealfu@gmail.com>
2022-05-08 23:38:11 -0500

    neftily deploy try 2

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/citas-vue-app/'
-    : '/',
   devServer: {
     proxy: 'https://citavehiculosapi2022.azurewebsites.net/api/Citas/',
 }
